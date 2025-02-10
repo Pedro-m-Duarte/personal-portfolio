eb68edf0b211d79a47d6fe9f958834274a02a5ca
Pedro Duarte <pdr.maia.d@gmail.com>
2025-02-10 17:02:39 -0300

    add portuguese infos

--- assets/js/script.js
+++ assets/js/script.js
@@ -145,7 +145,7 @@ for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
 
     for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+      if (this.getAttribute('data-translate') === pages[i].dataset.page) {
         pages[i].classList.add("active");
         navigationLinks[i].classList.add("active");
         window.scrollTo(0, 0);
@@ -156,4 +156,180 @@ for (let i = 0; i < navigationLinks.length; i++) {
     }
 
   });
+}
+
+
+function openWhatsApp() {
+  // Get the input values
+  const fullName = document.querySelector('[name="fullname"]').value;
+  const email = document.querySelector('[name="email"]').value;
+  const subject = document.querySelector('[name="subject"]').value;
+  const message = document.querySelector('[name="message"]').value;
+
+  // Format the WhatsApp message
+  const formattedMessage = `Olá! Me chamo ${fullName}!\nMeu email é ${email}\n\n${subject}\n\n${message}`;
+
+  // Create the WhatsApp URL with the pre-filled message
+  const encodedMessage = encodeURIComponent(formattedMessage);
+  const phoneNumber = "+5531998387711"; // Hardcoded phone number
+  const whatsappURL = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodedMessage}`;
+
+  // Open the WhatsApp URL in a new window or tab
+  window.open(whatsappURL, '_blank');
+}
+
+
+function sendEmail() {
+  // Get the input values
+  const fullName = document.querySelector('[name="fullname"]').value;
+  const email = document.querySelector('[name="email"]').value;
+  const subject = document.querySelector('[name="subject"]').value;
+  const message = document.querySelector('[name="message"]').value;
+
+  // Construct the mailto link with the form data
+  const body = encodeURIComponent(`Hello! My name is ${fullName}.\nMy email is ${email}.\n\n${message}`);
+  const mailtoLink = `mailto:pdr.maia.d@gmail.com?subject=${subject}&body=${body}`;
+
+  // Open the user's default email client with the mailto link
+  window.location.href = mailtoLink;
+}
+
+// Example of triggering translation on language change
+const languageButtons = document.querySelectorAll('.translate-button');
+languageButtons.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    const selectedLanguage = event.currentTarget.getAttribute('data-language');
+    translateTo(selectedLanguage);
+  });
+});
+
+function translateTo(language) {
+  const elements = document.querySelectorAll('[data-translate]');
+  elements.forEach((element) => {
+    const key = element.getAttribute('data-translate');
+    element.textContent = translations[language][key];
+  });
+}
+
+const translations = {
+  en: {
+    title: "Software Engineer",
+    showContacts: "Show Contacts",
+    phone: "Phone",
+    birthday: "Birthday",
+    birthdayDate: "January 21, 1999",
+    location: "Location",
+    aboutMe: "About Me",
+    aboutText1: "I am a passionate software developer who thrives on solving complex problems and crafting efficient solutions. With strong communication skills, I excel in collaborative team environments. My primary focus is backend development, and I am particularly enthusiastic about distributed systems.",
+    aboutText2: "Throughout my career, I have worked across diverse domains and technologies, ranging from developing intricate APIs to building web frontend applications. I have also gained hands-on experience in DevOps and on-premise application deployment. Additionally, I have had the opportunity to take on roles such as technical leadership and product ownership, where I leveraged my expertise while continuously honing my skills. I am committed to constant improvement, whether by mastering new technologies or exploring innovative methods to boost efficiency. ",
+    aboutText3: "With three years of industry experience, I have been responsible for proposing and implementing digital solutions, developing APIs from scratch, creating new features, and maintaining legacy systems. I enjoy collaborating with fellow developers, learning from seasoned professionals, and, when possible, mentoring newcomers. I am always eager to embrace new technologies and tackle challenging problems head-on.",
+    whatDoing: "My Skills",
+    about: "About",
+    resume: "Resume",
+    portfolio: "Portfolio",
+    contact: "Contact",
+    backendText: "API development of complex solutions.",
+    frontendText: "Development of web applications.",
+    systemIntegrationTitle: "System Integration",
+    systemIntegrationText: "Integration of self developed and external services." ,
+    devopsText: "Management of sistems trhough  computing infrasruccture.",
+    relationshipText: "Great relationship and comunication with clients and coleges",
+    relationshipTitle: "Relationship",
+    clients: "Clients",
+    experience: "Experience",
+    bwtechRole: "Software Developer",
+    bwtechText1: "Fullstack developer working on multiple products. Key responsibilities included:",
+    bwtechText2: "Developing APIs using C# and Python;",
+    bwtechText3: "Building and maintaining web applications using React;",
+    bwtechText4: "Managing system integration and on-premise applications;",
+    bwtechText5: "Maintaining and optimizing CI/CD pipelines;",
+    bwtechText6: "Building and managing databases;",
+    bwtechText7: "Developing systems for monitoring service performance.",
+    commitJrRole1: "Software Developer",
+    commitJrText1: "Fullstack developer responsible for:",
+    commitJrText2: "Developing applications using React and Node.js;",
+    commitJrRole2: "Leadership Roles:",
+    commitJrText3: "Squad Leader and Product Owner;",
+    commitJrText4: "Created technical documentation and led development teams using agile methodologies.",
+    commitJrRole3: "Sales Consulting Analyst:",
+    commitJrText5: "Generated leads and guided customers through their journey;",
+    commitJrText6: "Analyzed customer needs and recommended suitable products/services.",
+    cemighRole: "Internship in Electrical Energy Distribution",
+    cemigText1: "Worked within a team responsible for:",
+    cemigText2: "Studying electrical energy distribution and developing technical standards.",
+    cemigText3: "Preparing technical reports and assisting in research and development activities.",
+    cemigText4: "Providing technical support to other departments.",
+    education: "Education",
+    educationText1: "Bachelor of Computer Engineering",
+    educationText2: "Electroeletronics Technician",
+    mySkills: "My Skills",
+    languages: "Languages",
+    frameworks: "Frameworks and Development Tools",
+    others: "Others",
+    agileMethodologies: "Agile Methodologies",
+    communication: "communication",
+    personalProjects:"Personal projects" ,
+    contactForm: "Contact Form",
+    sendMessage: "Send Message",
+  },
+  pt: {
+    title: "Engenheiro de Software",
+    showContacts: "Mostrar Contatos",
+    phone: "Telefone",
+    birthday: "Aniversário",
+    birthdayDate: "21 de janeiro de 1999",
+    location: "Localização",
+    aboutMe: "Sobre Mim",
+    aboutText1: "Sou um desenvolvedor de software apaixonado que prospera ao resolver problemas complexos e criar soluções eficientes. Com fortes habilidades de comunicação, me destaco em ambientes de equipe colaborativos. Meu foco principal é o desenvolvimento de backend, e sou particularmente entusiasmado com sistemas distribuídos.",
+    aboutText2: "Ao longo da minha carreira, trabalhei em diversos domínios e tecnologias, desde o desenvolvimento de APIs complexas até a construção de aplicações web frontend. Também adquiri experiência prática em DevOps e implantação de aplicações on-premise. Além disso, tive a oportunidade de assumir funções como liderança técnica e propriedade de produto, onde utilizei minha expertise enquanto aprimorava minhas habilidades. Estou comprometido com a melhoria constante, seja dominando novas tecnologias ou explorando métodos inovadores para aumentar a eficiência.",
+    aboutText3: "Com três anos de experiência na indústria, fui responsável por propor e implementar soluções digitais, desenvolver APIs do zero, criar novas funcionalidades e manter sistemas legados. Gosto de colaborar com outros desenvolvedores, aprender com profissionais experientes e ,sempre que possível, ajudar iniciantes. Estou sempre ansioso para adotar novas tecnologias e enfrentar problemas desafiadores de frente.",
+    whatDoing: "Minhas Habilidades",
+    about: "Sobre",
+    resume: "Currículo",
+    portfolio: "Portfólio",
+    contact: "Contato",
+    backendText: "Desenvolvimento de APIs para soluções complexas.",
+    frontendText: "Desenvolvimento de aplicações web.",
+    systemIntegrationTitle: "Integração de Sistemas",
+    systemIntegrationText: "Integração de serviços desenvolvidos internamente e externos.",
+    devopsText: "Gerenciamento de sistemas através de infraestrutura de computação.",
+    relationshipText: "Ótimo relacionamento e comunicação com clientes e colegas.",
+    relationshipTitle: "Relacionamento",
+    clients: "Clientes",
+    experience: "Experiência",
+    bwtechRole: "Desenvolvedor de Software",
+    bwtechText1: "Desenvolvedor Fullstack trabalhando em múltiplos produtos. Principais responsabilidades:",
+    bwtechText2: "Desenvolvimento de APIs usando C# e Python;",
+    bwtechText3: "Construção e manutenção de aplicações web usando React;",
+    bwtechText4: "Gerenciamento de integração de sistemas e aplicações on-premise;",
+    bwtechText5: "Manutenção e otimização de pipelines de CI/CD;",
+    bwtechText6: "Construção e gerenciamento de bancos de dados;",
+    bwtechText7: "Desenvolvimento de sistemas para monitoramento de desempenho de serviços.",
+    commitJrRole1: "Desenvolvedor de Software",
+    commitJrText1: "Desenvolvedor Fullstack responsável por:",
+    commitJrText2: "Desenvolvimento de aplicações usando React e Node.js;",
+    commitJrRole2: "Funções de Liderança:",
+    commitJrText3: "Líder de Squad e Product Owner;",
+    commitJrText4: "Criação de documentação técnica e liderança de equipes de desenvolvimento usando metodologias ágeis.",
+    commitJrRole3: "Analista de Consultoria de Vendas:",
+    commitJrText5: "Geração de leads e orientação de clientes em sua jornada;",
+    commitJrText6: "Análise das necessidades dos clientes e recomendação de produtos/serviços adequados.",
+    cemighRole: "Estágio em Distribuição de Energia Elétrica",
+    cemigText1: "Trabalhei em uma equipe responsável por:",
+    cemigText2: "Estudar a distribuição de energia elétrica e desenvolver padrões técnicos.",
+    cemigText3: "Elaborar relatórios técnicos e auxiliar em atividades de pesquisa e desenvolvimento.",
+    cemigText4: "Prestar suporte técnico a outros departamentos.",
+    education: "Formação",
+    educationText1: "Bacharel em Engenharia de Computação",
+    educationText2: "Técnico em Eletroeletrônica",
+    mySkills: "Minhas Habilidades",
+    languages: "Linguagens",
+    frameworks: "Frameworks e Ferramentas de Desenvolvimento",
+    others: "Outros",
+    communication: "Comunicação",
+    agileMethodologies: "Metodologias Ágeis",
+    personalProjects: "Projetos Pessoais",
+    contactForm: "Formulário de Contato",
+    sendMessage: "Enviar Mensagem"
+  }
 }
